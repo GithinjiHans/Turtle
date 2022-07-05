4a12e862677e80438f887fa80e67eb03809d05f6
GithinjiHans <hanson4e@gmail.com>
2022-07-06 00:16:57 +0300

    Solved merge conflicts

--- app/src/main/java/com/githinjiHans/chatapp/SetupProfileActivity.kt
+++ app/src/main/java/com/githinjiHans/chatapp/SetupProfileActivity.kt
@@ -6,45 +6,119 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.githinjiHans.chatapp.databinding.ActivitySetupProfileBinding
+import com.githinjiHans.chatapp.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 
 class SetupProfileActivity : AppCompatActivity() {
 
-    var binding: ActivitySetupProfileBinding? = null
-    var auth:FirebaseAuth? = null
-    var database:FirebaseDatabase? = null
-    var storage:FirebaseStorage? = null
+    private var binding: ActivitySetupProfileBinding? = null
+    var auth: FirebaseAuth? = null
+    var database: FirebaseDatabase? = null
+    var storage: FirebaseStorage? = null
     var selectedImage: Uri? = null
-    var dialog:ProgressDialog? = null
+    var dialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySetupProfileBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
-        dialog!!.setMessage("Updating Profile...")
-        dialog!!.setCancelable(false)
+        dialog?.setMessage("Updating Profile...")
+        dialog?.setCancelable(false)
+        database = FirebaseDatabase.getInstance()
         storage = FirebaseStorage.getInstance()
         auth = FirebaseAuth.getInstance()
         supportActionBar?.hide()
-        binding!!.imageView.setOnClickListener{
+        binding!!.imageView.setOnClickListener {
             val intent = Intent()
-            intent.action  = Intent.ACTION_GET_CONTENT
+            intent.action = Intent.ACTION_GET_CONTENT
             intent.type = "image/*"
             startActivityForResult(intent, 45)
 
         }
-        binding!!.continueBtn1.setOnClickListener{
-            val name : String = binding!!.nameContainer.toString()
+        binding!!.continueBtn1.setOnClickListener {
+            val name: String = binding!!.nameContainer.text.toString()
             if (name.isEmpty()) {
-                binding!!.nameContainer.setError("Please enter a name")
+                binding!!.nameContainer.error = "Please enter a name"
             }
-            dialog!!.show()
+            dialog?.show()
             if (selectedImage != null) {
                 val reference = storage!!.reference.child("Profile")
                     .child(auth!!.uid!!)
+                reference.putFile(selectedImage!!).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        reference.downloadUrl.addOnCompleteListener { uri ->
+                            val imageUrl = uri.toString()
+                            val uid = auth!!.uid
+                            val phone = auth!!.currentUser!!.phoneNumber
+                            val name = binding!!.nameContainer.text.toString()
+                            val user = User(uid, name, phone, imageUrl)
+                            database!!.reference
+                                .child("users")
+                                .child(uid!!)
+                                .setValue(user)
+                                .addOnCompleteListener {
+                                    dialog?.dismiss()
+                                    val intent =
+                                        Intent(this@SetupProfileActivity, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                        }
+                    } else {
+                        val uid = auth!!.uid
+                        val phone = auth!!.currentUser!!.phoneNumber
+                        val name = binding!!.nameContainer.text.toString()
+                        val user = User(uid, name, phone, "No Image")
+                        database!!.reference
+                            .child("users")
+                            .child(uid!!)
+                            .setValue(user)
+                            .addOnCompleteListener {
+                                dialog?.dismiss()
+                                val intent = Intent(
+                                    this@SetupProfileActivity,
+                                    MainActivity::class.java
+                                )
+                                startActivity(intent)
+                                finish()
+                            }
+                    }
+                }
             }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null) {
+            if (data.data != null) {
+                val uri =data.data //filePath
+                val storage = FirebaseStorage.getInstance()
+                val time = Date().time
+                val reference = storage.reference
+                    .child("Profile")
+                    .child(time.toString() + "")
+                reference.putFile(uri!!).addOnCompleteListener{task ->
+                    if (task.isSuccessful){
+                        reference.downloadUrl.addOnCompleteListener{
+                            val filePath = uri.toString()
+                            val obj = HashMap<String,Any>()
+                            obj["image"]=filePath
+                            database!!.reference
+                                .child("users")
+                                .child(FirebaseAuth.getInstance().uid!!)
+                                .updateChildren(obj).addOnSuccessListener {  }
+                                }
+                        }
+                    }
+                binding!!.imageView.setImageURI(data.data)
+                selectedImage = data.data
+                }
+            }
+        }
 }
+
